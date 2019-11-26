4f367a33da77a7e9f5911c60d4de113496da0e9c
raphaeljq <raphaeljq@gmail.com>
2019-11-26 17:35:13 -0300

    adicionados arquivos do template parallax

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+<p>01- ABERTURA : acolhida, canto, intençoes, oracao do espirito santo e sinal da cruz; </p>
+
+<p>02-Oração pela igreja e pelos sarcedotes:</p>
+<p>"Ó meu Jesus, peço-vos por toda a Igreja; concedei-lhe o amor e a luz do Espírito Santo; dai força às palavras dos sacerdotes, de modo que rompam mesmo os corações mais endurecidos e os façam retornar a vós, ó Senhor. Senhor, dai-nos santos sacerdotes e vós mesmo conservai-os em santidade. Ó Divino e Sumo Sacerdote que o poder da vossa Misericórdia os acompanhe por onde andarem e os proteja contra as maquinações que o demônio não cessa de armar às almas de cada sacerdote. O poder de vossa Misericórdia, ó Senhor, destrua tudo o que poderá ofuscar a santidade do sacerdote, porque vós tudo podeis. Peço-vos, Jesus, abençoai com uma luz especial os sacerdotes com os quais me devo confessar durante minha vida. Amém."</p>
+
+<p>03-Via Sacra da Divina Misericordia - Eterno Pai, eu vos ofereço a dolorosa paixão de Jesus, em expiação dos nossos pecados e do mundo inteiro.</p>
+<p> Por sua condenação à morte, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela Cruz que lhe foi posta sobre os ombros, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela sua 1ª queda, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pelas lágrimas de sua mãe, que veio ao seu encontro, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pelo seu cansaço atroz, devido ao qual obrigaram um homem a ajudá-lo, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela compaixão da mulher que enxugou o seu rosto ensanguentado , tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela sua 2ª queda, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pelas palavras dirigidas as mulheres que dele se compadeciam, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela sua 3ª queda, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela dor que sentiu, quando o seu corpo foi despido, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela horrível perfuração dos pregos, que atravessaram suas mãos e os pés, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela água e o sangue emanados do seu coração, como fonte de todos os bens, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela lembrança de todos os tormentos de sua mãe quando o recebeu morto entre os braços, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela pedra que fechou o sepulcro, tende misericórdia de nós e do mundo inteiro.</p>
+<p> Pela sua gloriosa ressurreição e aparição aos seus discípulos, comunicando-lhes a paz, tende misericórdia de nós e do mundo inteiro.</p>
+<p>Vós morrestes, Jesus mas uma fonte de vida jorrou para as almas, e abriu-se um mar de misericórdia Divina envolvei o mundo todo e derramai-vos sobre nós.</p>
+<p>A pedra fechou o sepulcro mas não para sempre. Renovemos diante dele a nossa fé na divindade de Cristo e na sua infinita misericórdia. Ela nos fará ressuscitar espiritualmente dos nossos pecados, como também nos conduzirá à completa ressurreição de todo o nosso ser na vida eterna.</p>
+<p>(Um Pai nosso, uma Ave-Maria e um Glória ao Pai, segundo as intenções do Santo Padre)</p>
+
+
+<p>04 - Terço da Divina Misericórdia</p>
+
+<p>Oração Inicial Pai-Nosso... Ave-Maria... Creio... Nas contas grandes (do Pai-Nosso), reza-se: Eterno Pai, eu Vos ofereço o Corpo e Sangue, Alma e Divindade de Vosso diletíssimo Filho, Nosso Senhor Jesus Cristo, em expiação dos nossos pecados e dos do mundo inteiro. Nas contas pequenas (da Ave-Maria), reza-se: Pela Sua dolorosa Paixão, tende misericórdia de nós e do mundo inteiro. Ao final do terço, reza-se: Deus Santo, Deus Forte, Deus Imortal, tende piedade de nós e do mundo inteiro.</p>
+<p>"Desejo que conheças mais a fundo o Meu amor, de que está inflamado o Meu Coração pelas almas, e compreenderás isso quando refletires sobre a Minha Paixão. Invoca a Minha misericórdia para com os pecadores, pois desejo a salvação deles. Quando de coração contrito e confiante rezares essa oração por algum pecador, Eu lhe darei a graça da conversão. Esta pequena prece é a seguinte: - Ó Sangue e Água que jorrastes do Coração de Jesus como fonte de misericórdia para nós, eu confio em Vós" (Diário, 187).</p>
+
+<p>05 - Louvores a Misericórdia de Deus</p>
+
+<p>O amor de Deus é a flor e a misericórdia o fruto.Que a alma que desconfia leia estes louvores da misericórdia e torne-se confiante.</p>
+<p>>Misericórdia Divina, que brota do seio do Pai, eu confio em Vós. Misericórdia Divina, atributo máximo de Deus, eu confio em Vós. Misericórdia Divina, mistério inefável, eu confio em Vós. Misericórdia Divina, fonte que brota do mistério da Santíssima Trindade, eu confio em Vós. Misericórdia Divina, que nenhuma mente, nem humana nem angélica pode perscrutar, eu confio em Vós. Misericórdia Divina, da qual provém toda a vida e felicidade, eu confio em Vós. Misericórdia Divina, mais sublime do que os Céus, eu confio em Vós. Misericórdia Divina, fonte de milagres e prodígios, eu confio em Vós. Misericórdia Divina, que envolve o universo todo, eu confio em Vós. Misericórdia Divina, que desce ao mundo na Pessoa do Verbo Encarnado, eu confio em Vós.</p>
+<p>Misericórdia Divina, que brotou da chaga aberta do Coração de Jesus, eu confio em Vós. Misericórdia Divina, encerrada no Coração de Jesus para nós e sobretudo para os pecadores, eu confio em Vós. Misericórdia Divina, imperscrutável na instituição da Eucaristia, eu confio em Vós. Misericórdia Divina, na instituição da Santa Igreja, eu confio em Vós. Misericórdia Divina, no sacramento do Santo Batismo, eu confio em Vós. Misericórdia Divina, na nossa justificação por Jesus Cristo, eu confio em Vós. Misericórdia Divina, que nos acompanha por toda a vida, eu confio em Vós. Misericórdia Divina, que nos envolve de modo particular na hora da morte, eu confio em Vós. Misericórdia Divina, que nos concede a vida imortal, eu confio em Vós.</p>
+<p>Misericórdia Divina, que nos acompanha em todos os momentos da vida, eu confio em Vós. Misericórdia Divina, que nos defende do fogo do Inferno, eu confio em Vós. Misericórdia Divina, na conversão dos pecadores endurecidos, eu confio em Vós. Misericórdia Divina, enlevo para os anjos, inefável para os Santos, eu confio em Vós. Misericórdia Divina, insondável em todos os mistérios divinos, eu confio em Vós. Misericórdia Divina, que nos eleva de toda miséria, eu confio em Vós. Misericórdia Divina, fonte de nossa felicidade e alegria, eu confio em Vós. Misericórdia Divina, que do nada nos chama para a existência, eu confio em Vós. Misericórdia Divina, que abrange todas as obras das Suas mãos, eu confio em Vós. Misericórdia Divina, que coroa tudo que existe e que existirá, eu confio em Vós. Misericórdia Divina, na qual todos somos imersos, eu confio em Vós. Misericórdia Divina, doce consolo para os corações atormentados, eu confio em Vós. Misericórdia Divina, única esperança dos desesperados, eu confio em Vós. Misericórdia Divina, repouso dos corações, paz em meio ao terror, eu confio em Vós. Misericórdia Divina, delícia e êxtase dos Santos, eu confio em Vós. Misericórdia Divina, que desperta a confiança onde não há esperança, eu confio em Vós.</p>
+<p>“ Deus eterno, em quem a misericórdia é insondável e o tesouro da compaixão é inesgotável, olhai propício para nós e multiplicai em nós a Vossa misericórdia, para que não desesperemos nos momentos difíceis, nem esmoreçamos, mas nos submetamos com grande confiança à Vossa santa vontade, que é Amor e a própria Misericórdia" (Diário, 949).</p>
+<p>“A humanidade não encontrará a paz enquanto não se voltar, com confiança, para a minha misericórdia.” (Diário,300)</p>
+
+<p>06 - Oração à divina misericórdia</p>
+
+<p>Ó Deus Eterno, em quem a Misericórdia é insondável, e o tesouro da compaixão é inesgotável, olhai propício para nós e multiplicai em nós a vossa Misericórdia, para que não desesperemos nos momentos difíceis, nem esmoreçamos, mas nos submetamos com grande confiança à Vossa Santa Vontade, que é amor e a própria Misericórdia. Amém.”</p>
+
+<p>07 - Mensagem do livro da Santa Faustina</p>
+
+<p>No final reza-se a prece:</p>
+<p>"Ó sangue e Água que jorrastes do coração de Jesus como fonte de misericordia para nós, eu confio em vós" (Diário, 187)</p>
+
+<p>08 - Oração de agradecimento</p>
+
+<p>Ó JESUS, Ó DEUS Eterno, Te agradeço pelos inúmeros benefícios e por Tuas graças. Que cada batida do meu Coração seja um hino de agradecimento a Ti, ó DEUS. Que as gotas do meu Sangue sejam circuladas por Ti, ó Senhor. A minha Alma é um canto de adoração à Tua misericórdia. Amo-Te, Ó DEUS, por todo o sempre, por toda a Vida.</p>
+<p>Amém...</p>
+
+<p>09 - Benção Final</p>
+
+<p>Santa Faustina, Rogai por nós! São João Paulo Segundo, Rogai por nós! São Benedito, Rogai por nós! Nossa Senhora Mãe da Misericórdia! Rogai por nós!</p>
+<p>Em nome do pai ...</p>
+    </div>
+  );
+}
+
+export default App;
